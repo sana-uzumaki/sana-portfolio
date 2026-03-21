113e420ca2ef2b328bb1ee1e751781a185ea7431
Sanantheshwaran E <sana0630c@gmail.com>
2026-03-22 00:07:27 +0530

    link

--- src/contacts.jsx
+++ src/contacts.jsx
@@ -7,8 +7,8 @@ export default function Contact() {
                 <section id="contacts" className='flex-1 p-6 m-2 ml-5 border border-slate-600  bg-slate-800 rounded-2xl m-4 shadow-white hover:shadow-xl/10'>
                     <h2 className="font-extrabold text-xl text-center m-5 mb-6">Contact</h2>
                     <p className="bg-slate-400/20 border border-slate-400 rounded p-2 m-2">Email : <a href="sana0630c@gmail.com">sana0630c@gmail.com</a></p>
-                    <p className="bg-blue-400/20 border border-blue-400 rounded p-2 m-2">GitHub: <a href="https://github.com/yourusername">sana._uzumaki</a> </p>
-                    <p className="bg-yellow-400/20 border border-yellow-400 rounded p-2 m-2">LinkedIn: <a href="https://linkedin.com/in/yourusername">sanantheshwaran E</a> </p>
+                    <p className="bg-blue-400/20 border border-blue-400 rounded p-2 m-2">GitHub: <a href="https://github.com/sana-uzumaki">sana-uzumaki</a> </p>
+                    <p className="bg-yellow-400/20 border border-yellow-400 rounded p-2 m-2">LinkedIn: <a href="https://linkedin.com/in/sanantheshwaran-e">sanantheshwaran E</a> </p>
                 </section>
                 <section className='flex-1 p-6 m-2 mr-5 border border-slate-600 bg-slate-800 rounded-2xl m-4 shadow-white hover:shadow-xl/10'>
                     <p className='text-lg'>I'm curretly looking for internship opportunities and real-world projecs where can imporve my skills and contribute to meaningful work.
