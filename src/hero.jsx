6106689ef282bb4ed3120a7127609498eb811031
sana-uzumaki <sana0630c@gmail.com>
2026-03-25 20:19:25 +0530

    correct the spelling

--- src/hero.jsx
+++ src/hero.jsx
@@ -9,8 +9,8 @@ export default function Hero() {
                     <h1 className='text-4xl mb-4'>Hello, I'm
                         <br />
                         sanantheswharan</h1>
-                    <p className='text-xl mb-4'>Computer Science Enineering Student | Aspiring Web Developer</p>
-                    <p className='text-lg'>I am  2nd-year CSE student at Muthayammal Engineering College. I build responsive web applications using React and Twilwind CSS and practice Data Structures and Algorithms to improve my problem Solving skills. I am currently looking for opportunities to learn, buid real-world projects, and grow as a developer.</p>
+                    <p className='text-xl mb-4'>Computer Science Engineering Student | Aspiring Web Developer</p>
+                    <p className='text-lg'>I am  2nd-year CSE student at Muthayammal Engineering College. I build responsive web applications using React and Tailwind CSS and practice Data Structures and Algorithms to improve my problem Solving skills. I am currently looking for opportunities to learn, buid real-world projects, and grow as a developer.</p>
                 </section>
                 <section className='flex md:flex-1 justify-center items-center'>
                     <img src=""  className='rounded h-100 w-100'/>
