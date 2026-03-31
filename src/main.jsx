efeb39d9781cca35c9f82b0c9b60e69378dc124f
sana-uzumaki <sana0630c@gmail.com>
2026-03-31 21:15:50 +0530

    del portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Portfolio from './portfolio.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Portfolio />
+    <App />
   </StrictMode>,
 )
